c10a7df9f5012b808e002cee6ad4484653691657
YuriDlcrz <135201573+YuriDlcrz@users.noreply.github.com>
2024-12-28 20:53:56 +0800

    Update student.service.ts

--- student/student.service.ts
+++ student/student.service.ts
@@ -10,8 +10,8 @@ export class StudentService {
     private readonly studentRepository: Repository<Student>,
   ) {}
 
- async findAll(): Promise<Student[]> {
-    return await this.studentRepository.find();
+async update(id: number, student: Partial<Student>): Promise<Student> {
+    await this.studentRepository.update(id, student);
+    return await this.studentRepository.findOne({ where: { id } });
   }
-
 }
